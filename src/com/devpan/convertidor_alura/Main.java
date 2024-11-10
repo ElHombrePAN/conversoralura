9dab6727fee303ce0d5be377999cf46f3fc249c4
DevPan <moisesrb18@gmail.com>
2024-11-10 12:59:48 -0600

    Opciones de conversion - cantidad y monedas disponibles

--- src/com/devpan/convertidor_alura/Main.java
+++ src/com/devpan/convertidor_alura/Main.java
@@ -2,14 +2,60 @@ package com.devpan.convertidor_alura;
 
 import com.fasterxml.jackson.databind.JsonNode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
         ApiRequest apiRequest = new ApiRequest();
-        JsonNode datosPar = apiRequest.getDatosPar("USD", "MXN");
-        if (datosPar != null && datosPar.has("conversion_rate")) {
-            System.out.println("Tasa de conversion USD a MXN es de: " + datosPar.get("conversion_rate").asDouble());
+        Scanner scanner = new Scanner(System.in);
+        double monto;
+        List<String> monedasPermitidas  = Arrays.asList("ARS", "BOB", "BRL", "CLP", "COP", "USD", "MXN");
+
+        System.out.println("CONVERSOR DE MONEDAS\nLas monedas disponibles son las siguientes elige una: \n");
+        System.out.println("Peso Argentino => ARS \nPeso Mexicano => MXN \nBoliviano => BOB" +
+                "\nReal Brasileño => BRL \nPeso Chileno => CLP \nPeso Colombiano => COP" +
+                "\nDolar Estadounidense => USD\n");
+
+
+        String monedaBase;
+        while (true){
+            System.out.println("Ingresa una moneda base: ");
+            monedaBase = scanner.nextLine().toUpperCase();
+            if (monedasPermitidas.contains(monedaBase)){
+                break;
+            } else {
+                System.out.println("Moneda base no permitida por favor usa una de las siguientes " + monedasPermitidas + "\n");
+            }
         }
+
+
+        String monedaDestino;
+        while (true){
+            System.out.println("Ingresa una moneda destino: ");
+            monedaDestino = scanner.nextLine().toUpperCase();
+            if (monedasPermitidas.contains(monedaDestino)){
+                break;
+            } else {
+                System.out.println("Moneda base no permitida por favor usa una de las siguientes " + monedasPermitidas + "\n");
+            }
+        }
+
+
+        System.out.println("¿Que cantidad quieres convertir?");
+        int cantidad = scanner.nextInt();
+
+        JsonNode datosPar = apiRequest.getDatosPar(monedaBase, monedaDestino);
+        if (datosPar != null && datosPar.has("conversion_rate")) {
+            System.out.println("La tasa de conversion " + monedaBase + " a " + monedaDestino + " es de: "
+                    + datosPar.get("conversion_rate").asDouble());
+        }
+
+        monto = cantidad * datosPar.get("conversion_rate").asDouble();
+        System.out.println(cantidad + " " + monedaBase + " a " + monedaDestino + " equivalen a " + monto);
 
     }
 
